14e53f2c7c6ab46cc6dd204bee847229729bc4db
Bimo AR <bimoarta@gmail.com>
2022-01-25 14:47:39 +0700

    adding register route on app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require(`express`);
 const app = express();
 const port = 3000;
 const cors = require(`cors`);
+const Cont = require(`./controllers/controllers`);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post(`/register`);
+app.post(`/register`, Cont.register);
