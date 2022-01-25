9c2d94c830ecabb0d561e75fcd1fe06fdf2c78ff
JulluisW <julluisw@gmail.com>
2022-01-25 22:39:31 +0700

    shop register done

--- models/shop.js
+++ models/shop.js
@@ -14,12 +14,41 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Shop.init({
-    name: DataTypes.STRING,
-    userId: DataTypes.INTEGER,
+    name: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      unique: {
+        msg: "Entered Shop name has been taken, please enter another name"
+      },
+      validate: {
+        notNull: {
+          msg : "Shop name is required"
+        },
+        notEmpty:{
+          msg : "Shop name is required"
+        },
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
     imageUrl: DataTypes.STRING,
     motto: DataTypes.STRING,
     address: DataTypes.STRING,
-    category: DataTypes.STRING
+    category: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      validate: {
+        notNull: {
+          msg : "Category is required"
+        },
+        notEmpty:{
+          msg : "Category is required"
+        },
+      }
+    }
   }, {
     sequelize,
     modelName: 'Shop',
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const indexRouter = express.Router()
 const userRouter = require('./userRouter.js')
 const shopRouter = require('./shopRouter.js')
+const Authentication = require('../Middlewares/Authentication.js')
 
 indexRouter.use('/', userRouter)
 
-
+indexRouter.use(Authentication)
 
 indexRouter.use('/shop', shopRouter)
 
