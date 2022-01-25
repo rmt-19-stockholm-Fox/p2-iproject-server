0c4f21ba19217cd5792312d285d1d741064eedec
Daniel Levyson <levyson.daniel@gmail.com>
2022-01-25 15:02:00 +0700

    implement API for deleeting post

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ router.post('/login-google', controller.loginGoogle);
 router.post('/user', controller.getUserByToken);
 router.post('/posts', auth, controller.createPost);
 router.get('/posts', controller.getPosts);
+router.delete('/posts/:id', controller.deletePost);
 
 module.exports = router;
