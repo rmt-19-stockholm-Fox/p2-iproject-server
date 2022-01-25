cab768f5f14f8592b0e4ca71ceb5a045e0780147
JulluisW <julluisw@gmail.com>
2022-01-25 16:13:46 +0700

    Authentication done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const indexRouter = express.Router()
 const userRouter = require('./userRouter.js')
-
+const shopRouter = require('./shopRouter.js')
 
 indexRouter.use('/', userRouter)
 
+
+
+indexRouter.use('/shop', shopRouter)
 
 module.exports = indexRouter
