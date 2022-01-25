ba401b9ab953924ff238d9a6eff675c14e15054c
JulluisW <julluisw@gmail.com>
2022-01-25 23:35:14 +0700

    edit product feature done and revision on add product

--- models/product.js
+++ models/product.js
@@ -18,14 +18,64 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        
+        notNull:{
+          msg: "Product name is required"
+        },
+        notEmpty: {
+          msg: "Product name is required"
+        }
       }
     },
-    shopId: DataTypes.INTEGER,
-    description: DataTypes.STRING,
-    price: DataTypes.INTEGER,
+    shopId:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull:{
+          msg: "Shop Id is required"
+        },
+        notEmpty: {
+          msg: "Shop Id is required"
+        }
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull:{
+          msg: "Description is required"
+        },
+        notEmpty: {
+          msg: "Description is required"
+        },
+      }
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull:{
+          msg: "Price is required"
+        },
+        notEmpty: {
+          msg: "Price is required"
+        },
+        min: 1
+      }
+    },
     bulkPrice: DataTypes.INTEGER,
-    brand: DataTypes.STRING
+    brand: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull:{
+          msg: "Brand is required"
+        },
+        notEmpty: {
+          msg: "Brand is required"
+        },
+      }
+    }
   }, {
     sequelize,
     modelName: 'Product',
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -6,13 +6,38 @@ productRouter.post('/add', async(req,res,next) => {
   try {
     const {name, description, price, bulkPrice, brand, shopId} = req.body;
 
-    Product.create({
+    const newProduct = await Product.create({
       name,
       description,
       price,
       bulkPrice,
       brand,
       shopId,
+    })
+    res.status(201).json(newProduct)
+  } catch (error) {
+    next(error)
+  }
+})
+
+productRouter.put('/edit/:productId', async(req,res,next) => {
+  try {
+    const {name, description, price, bulkPrice, brand} = req.body;
+    const {productId} = req.params;
+
+    await Product.update({
+      name,
+      description,
+      price,
+      bulkPrice,
+      brand,
+    },{
+      where: {
+        id: productId
+      }
+    })
+    res.status(200).json({
+      message: "Product update success"
     })
   } catch (error) {
     next(error)
