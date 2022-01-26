397a07cc712ee50655dbe585e8c73c139025cad2
glenn2206 <leonardusglenn@gmail.com>
2022-01-26 17:46:31 +0700

    deploy

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandles')
 const route = require('./routes')
