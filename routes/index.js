7e9b12dd19cb70e4e3ad18a29beb00db99c92630
Daniel Levyson <levyson.daniel@gmail.com>
2022-01-27 10:45:42 +0700

    fix some status code and response message of some APIs

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/users/:id', controller.getUserById);
 router.post('/user', controller.getUserByToken);
 router.post('/posts', auth, uploader, controller.createPost);
 router.get('/posts', controller.getPosts);
-router.delete('/posts/:id', controller.deletePost);
+router.delete('/posts/:id', auth, controller.deletePost);
 router.get('/places/photo', controller.getPhoto);
 router.get('/places/:id', controller.getPlaceDetail);
 router.get('/places', auth, controller.searchPlaces);
