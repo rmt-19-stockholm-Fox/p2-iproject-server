85fddc80c48bb7931c564b9395e0d69a9c525a2c
Bimo AR <bimoarta@gmail.com>
2022-01-27 11:00:18 +0700

    deploying

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const express = require(`express`);
 const app = express();
 const cors = require(`cors`);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const Cont = require(`./controllers/controller.js`);
 const errhand = require(`./middlewares/errhand`);
 
